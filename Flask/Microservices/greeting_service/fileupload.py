2133b4a590c054b9d4502e2474216dc99869e1b1
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2023-10-31 06:59:22 +0000

    Added microservices

---
+++ Flask/Microservices/greeting_service/fileupload.py
@@ -0,0 +1,33 @@
+from flask import request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '/path/to/upload/folder'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload a File</title>
+    <h1>Upload File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(port=5000)
